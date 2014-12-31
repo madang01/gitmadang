bf0ef844df3a3c03a9ef51a7b6cb4f3921b7b830
Won Jonghoon <k9200544@hanmail.net>
2014-12-31 20:25:59 +0900

    v5.0.52 config var work v8
    v5.0.52 config var work v8

---
+++ sinnori/core_build/APP-INF/src/kr/pe/sinnori/impl/server/mybatis/SqlSessionFactoryManger.java
@@ -0,0 +1,531 @@
+package kr.pe.sinnori.impl.server.mybatis;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import kr.pe.sinnori.common.configuration.ServerProjectConfig;
+import kr.pe.sinnori.common.configuration.SinnoriConfig;
+import kr.pe.sinnori.common.exception.DBNotReadyException;
+import kr.pe.sinnori.common.lib.CommonStaticFinalVars;
+import kr.pe.sinnori.common.lib.DBCPManager;
+import kr.pe.sinnori.common.lib.LastModifiedFileInfo;
+import kr.pe.sinnori.server.classloader.ServerClassLoader;
+
+import org.apache.ibatis.datasource.unpooled.UnpooledDataSourceFactory;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * <pre>
+ * MyBatis SqlSessionFactory 관리자
+ * 
+ * 신놀이는 MyBatis 매핑 클래스들을 동적 클래스를 통해 적재 되어야 하는 대상으로 정의했다.
+ * 이를 만족 시키기 위해서 MyBatis 라이브러리를 동적 클래스 클래스를 통해 적재해야 했다.
+ * MyBatis 는 매핑 파일들을 클래스 로더로 찾는것은 시스템과 자신을 호출한 classpath 이다.
+ * 이런 제약  조건때문에 "MyBatis SqlSessionFactory 관리자" 역시 동적 클래스로 적재되어야 한다.
+ * </pre>
+ * @author Won Jonghoon
+ *
+ */
+public class SqlSessionFactoryManger {
+	private final Logger log = LoggerFactory
+			.getLogger(SqlSessionFactoryManger.class);
+
+	private ClassLoader serverClassLoader = this.getClass().getClassLoader();
+
+	private Hashtable<String, SqlSessionFactory> connectionPoolName2SqlSessionFactoryHash = new Hashtable<String, SqlSessionFactory>();
+
+	private Hashtable<String, String> alias2typeHash = new Hashtable<String, String>();
+
+	private List<LastModifiedFileInfo> lastModifiedFileInfoList = new ArrayList<LastModifiedFileInfo>();
+
+	/**
+	 * 동기화 쓰지 않고 싱글턴 구현을 위한 비공개 생성자.
+	 * 
+	 * @throws DBNotReadyException
+	 */
+	private SqlSessionFactoryManger() {
+		rebuild();
+	}
+
+	/**
+	 * 동기화 쓰지 않고 싱글턴 구현을 위한 비공개 클래스
+	 */
+	private static final class SqlSessionFactoryMangerHolder {
+		static final SqlSessionFactoryManger singleton = new SqlSessionFactoryManger();
+	}
+
+	/**
+	 * 동기화 쓰지 않는 싱글턴 구현 메소드
+	 * 
+	 * @return 싱글턴 객체
+	 */
+	public static SqlSessionFactoryManger getInstance() {
+		return SqlSessionFactoryMangerHolder.singleton;
+	}
+
+	@SuppressWarnings("unchecked")
+	private void rebuild() {
+		
+		
+		SinnoriConfig conf = SinnoriConfig.getInstance();	
+
+		List<String> dbcpConnectionPoolNameList = (List<String>) conf
+				.getResource("dbcp.connection_pool_name_list.value");
+
+		
+		
+		String workProjectName = null;
+		if (serverClassLoader instanceof ServerClassLoader) {
+			workProjectName = ((ServerClassLoader)serverClassLoader).getProjectName();
+		} else {
+			workProjectName = System.getProperty(CommonStaticFinalVars.SINNORI_PROJECT_NAME_JAVA_SYSTEM_VAR_NAME);
+		}
+		
+		ServerProjectConfig serverProjectConfig = conf.getServerProjectConfig(workProjectName);
+
+		File mybatisConfigeFile = serverProjectConfig.getMybatisFile();
+		/*String mybatisConfigeFilePathString = (String) conf.getResource("mybatis.config_file.value");
+		try {
+			URL mybatisConfigResourceURL = serverClassLoader
+					.getResource(mybatisConfigeFilePathString);
+
+			if (null ==  mybatisConfigResourceURL) {
+				log.warn(
+						new StringBuilder("the mybatis config file resource value[")
+								.append(mybatisConfigeFilePathString)
+								.append("] cannot be found").toString());
+				return;
+			}
+			
+						
+			mybatisConfigeFile = new File(mybatisConfigResourceURL.toURI());
+		} catch (Exception e) {
+			log.warn(
+					new StringBuilder("the mybatis config file resource value[")
+							.append(mybatisConfigeFilePathString)
+							.append("] fail to convert to file").toString(), e);
+			return;
+		}*/
+
+		connectionPoolName2SqlSessionFactoryHash.clear();
+		alias2typeHash.clear();
+		lastModifiedFileInfoList.clear();
+		{
+			LastModifiedFileInfo readFileInfo = new LastModifiedFileInfo(
+					mybatisConfigeFile, null);
+			lastModifiedFileInfoList.add(readFileInfo);
+		}
+
+		try {
+			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
+					.newInstance();
+			DocumentBuilder documentBuilder = documentBuilderFactory
+					.newDocumentBuilder();
+			Document doc = documentBuilder.parse(mybatisConfigeFile);
+
+			Element rootElement = doc.getDocumentElement();
+			rootElement.normalize();
+
+			// FIXME!
+			log.info("root element name[{}]", rootElement.getNodeName());
+
+			NodeList mapperNodeList = rootElement
+					.getElementsByTagName("mapper");
+			int len = mapperNodeList.getLength();
+
+			for (int i = 0; i < len; i++) {
+				org.w3c.dom.Node mapperNode = mapperNodeList.item(i);
+				// FIXME!
+				log.info("mapperNode[{}][{}]", mapperNode.getNodeName(),
+						mapperNode.getNodeType());
+
+				if (mapperNode.getNodeType() == org.w3c.dom.Node.ELEMENT_NODE) {
+					Element mapperElement = (Element) mapperNode;
+
+					if (mapperElement.getNodeName().equals("mapper")) {
+						String mapperResourceValue = mapperElement
+								.getAttribute("resource");
+
+						if (null == mapperResourceValue)
+							mapperResourceValue = "";
+						mapperResourceValue = mapperResourceValue.trim();
+
+						File mapperFile = null;
+
+						if (mapperResourceValue.equals("")) {
+							mapperResourceValue = mapperElement
+									.getAttribute("url");
+							if (null == mapperResourceValue)
+								mapperResourceValue = "";
+							mapperResourceValue = mapperResourceValue.trim();
+
+							if (mapperResourceValue.equals("")) {
+								/** mapper interface class */
+
+								mapperResourceValue = mapperElement
+										.getAttribute("class");
+								if (null == mapperResourceValue)
+									mapperResourceValue = "";
+								mapperResourceValue = mapperResourceValue
+										.trim();
+								if (mapperResourceValue.equals("")) {
+									log.warn(
+											"bad mybatis config file error::bad mapper resource[{}], "
+													+ "you need to choose one of 3 attributes(resource, url, class)",
+											mapperResourceValue);
+									return;
+								}
+
+								try {
+									serverClassLoader
+											.loadClass(mapperResourceValue);
+								} catch (ClassNotFoundException e) {
+									log.warn(
+											"ClassNotFoundException::mapperResourceValue={}",
+											mapperResourceValue);
+									return;
+								}
+
+								continue;
+							} else {
+								/** url fully qualified path */
+								URL url = null;
+								try {
+									url = new URL(mapperResourceValue);
+								} catch (MalformedURLException e) {
+									log.warn(
+											"bad mybatis config file error::bad mapper resource[{}], MalformedURLException={}",
+											mapperResourceValue, e.getMessage());
+									return;
+								}
+								URI uri = null;
+								try {
+									uri = url.toURI();
+								} catch (URISyntaxException e) {
+									log.warn(
+											"bad mybatis config file error::bad mapper resource[{}], URISyntaxException={}",
+											mapperResourceValue, e.getMessage());
+									return;
+								}
+
+								mapperFile = new File(uri);
+							}
+						} else {
+							/** classpath relative resource */
+							/*
+							 * String mapperFilePathString = new StringBuilder(
+							 * classLoaderAPPINFPathString)
+							 * .append(File.separator) .append("resources")
+							 * .append(File.separator)
+							 * .append(mapperResourceValue.replaceAll("/",
+							 * File.separator)).toString();
+							 * 
+							 * mapperFile = new File(mapperFilePathString);
+							 */
+
+							try {
+								URL mapperFileURL  = serverClassLoader.getResource(mapperResourceValue);
+								
+								if (null == mapperFileURL) {
+									log.warn(
+											new StringBuilder(
+													"the mapper file resource value[")
+													.append(mapperResourceValue)
+													.append("] cannot be found")
+													.toString());
+									return;
+								}
+																
+								mapperFile = new File(mapperFileURL.toURI());
+							} catch (Exception e) {
+								log.warn(
+										new StringBuilder(
+												"the mapper file resource value[")
+												.append(mapperResourceValue)
+												.append("] fail to convert to file")
+												.toString(), e);
+								return;
+							}
+						}
+
+						if (!mapperFile.exists()) {
+							log.warn(
+									"bad mybatis config file error::bad mapper resource[{}] file[{}] is not found",
+									mapperResourceValue,
+									mapperFile.getAbsolutePath());
+							return;
+						}
+
+						if (!mapperFile.isFile()) {
+							log.warn(
+									"bad mybatis config file error::bad mapper resource[{}] file[{}] is not a normal file",
+									mapperResourceValue,
+									mapperFile.getAbsolutePath());
+							return;
+						}
+
+						if (!mapperFile.canRead()) {
+							log.warn(
+									"bad mybatis config file error::bad mapper resource[{}] file[{}] cannot be read",
+									mapperResourceValue,
+									mapperFile.getAbsolutePath());
+							return;
+						}
+
+						if (!mapperFile.canWrite()) {
+							log.warn(
+									"bad mybatis config file error::bad mapper resource[{}] file[{}] cannot be written",
+									mapperResourceValue,
+									mapperFile.getAbsolutePath());
+							return;
+						}
+
+						LastModifiedFileInfo readFileInfo = new LastModifiedFileInfo(
+								mapperFile, null);
+
+						lastModifiedFileInfoList.add(readFileInfo);
+
+						/**
+						 * FIXME! 파일 수정 여부를 판단할 수 있는 정보를 담은 객체
+						 * 목록(lastModifiedFileInfoList)에 매핑 파일을 추가한다. mybatis 재
+						 * 로딩 여부는 파일 수정 여부를 판단할 수 있는 정보를 담은 객체 목록에 등록된 파일들의 수정
+						 * 여부로 판단한다.
+						 */
+						log.info("변경 여부 점검할 매핑 파일 추가, 매핑 파일 경로={}",
+								mapperFile.getAbsolutePath());
+					}
+				}
+			}
+
+			NodeList typeAliasNodeList = rootElement
+					.getElementsByTagName("typeAlias");
+			len = typeAliasNodeList.getLength();
+			for (int i = 0; i < len; i++) {
+				org.w3c.dom.Node typeAliasNode = typeAliasNodeList.item(i);
+				if (typeAliasNode.getNodeType() == org.w3c.dom.Node.ELEMENT_NODE) {
+					Element typeAliaseElement = (Element) typeAliasNode;
+
+					String alias = typeAliaseElement.getAttribute("alias");
+					String type = typeAliaseElement.getAttribute("type");
+
+					// FIXME!
+					log.info("node name={}, alias={}, type={}",
+							typeAliasNode.getNodeName(), alias, type);
+
+					alias2typeHash.put(alias, type);
+				}
+			}
+
+			NodeList environmentNodeList = rootElement
+					.getElementsByTagName("environment");
+			len = environmentNodeList.getLength();
+			for (int i = 0; i < len; i++) {
+				org.w3c.dom.Node environmentNode = environmentNodeList.item(i);
+				if (environmentNode.getNodeType() == org.w3c.dom.Node.ELEMENT_NODE) {
+					Element environmentElement = (Element) environmentNode;
+
+					String environmentID = environmentElement
+							.getAttribute("id");
+
+					// FIXME!
+					log.info("node name={}, environmentID={}",
+							environmentNode.getNodeName(), environmentID);
+
+					if (!dbcpConnectionPoolNameList.contains(environmentID)) {
+						log.warn(
+								"bad mybatis config file error::environment id[{}] is not same to dbcp conneection pool name[{}]",
+								environmentID,
+								dbcpConnectionPoolNameList.toString());
+						return;
+					}
+
+					NodeList dataSourceNodeList = environmentElement
+							.getElementsByTagName("dataSource");
+
+					Element dataSourceElement = (Element) dataSourceNodeList
+							.item(0);
+
+					String dataSourceClassName = dataSourceElement
+							.getAttribute("type");
+					String type = alias2typeHash.get(dataSourceClassName);
+					if (null != type) {
+						// FIXME!
+						log.info(
+								"environmentID[{}]'s dataSourceClassName[{}] is alias so change to value[{}]",
+								environmentID, dataSourceClassName, type);
+
+						dataSourceClassName = type;
+					}
+					Class<?> dataSourceClass = null;
+					try {
+						// dataSourceClass = Class.forName(dataSourceClassName);
+						dataSourceClass = serverClassLoader
+								.loadClass(dataSourceClassName);
+					} catch (ClassNotFoundException e1) {
+						log.warn(
+								"bad mybatis config file error::environmentID[{}]'s data source class not found[{}]",
+								environmentID, dataSourceClassName);
+						return;
+					}
+
+					Object resultObj = null;
+					try {
+						resultObj = dataSourceClass.newInstance();
+					} catch (InstantiationException e1) {
+						log.warn(
+								"bad mybatis config file error::fail to get environmentID[{}]'s data source class[{}] instance",
+								environmentID, dataSourceClassName);
+						return;
+					} catch (IllegalAccessException e1) {
+						log.warn(
+								"bad mybatis config file error::fail to get environmentID[{}]'s data source class[{}] "
+										+ "instance because of illegal access",
+								environmentID, dataSourceClassName);
+						return;
+					}
+
+					if (!(resultObj instanceof UnpooledDataSourceFactory)) {
+						log.warn(
+								"bad mybatis config file error::environmentID[{}]'s data source class[{}] is not a type of UnpooledDataSourceFactory class",
+								environmentID, dataSourceClassName);
+						return;
+					}
+
+					UnpooledDataSourceFactory unpooledDataSourceFactory = (UnpooledDataSourceFactory) resultObj;
+
+					String dbcpConnectionPoolName = DBCPManager.getInstance()
+							.getDBCPConnectionPoolName(
+									unpooledDataSourceFactory.getDataSource());
+					if (null == dbcpConnectionPoolName) {
+						log.warn(
+								"bad mybatis config file error::unknown environmentID[{}]'s data soruce",
+								environmentID);
+						return;
+					}
+
+					if (!dbcpConnectionPoolName.equals(environmentID)) {
+						log.warn(
+								"bad mybatis config file error::environmentID[{}]'s data soruce's dbcp connection pool name[{}] is bad, "
+										+ "the environment name must be a dbcp connection pool name",
+								environmentID, dbcpConnectionPoolName);
+						return;
+					}
+
+					SqlSessionFactory sqlSessionFactory = null;
+
+					InputStream is = null;
+					try {
+						is = new FileInputStream(mybatisConfigeFile);
+
+						try {
+							sqlSessionFactory = new SqlSessionFactoryBuilder()
+									.build(is, environmentID);
+						} catch (Exception e) {
+							e.printStackTrace();
+							log.warn(
+									"fail to get a SqlSessionFactory object from mybatis config file[{}] having environment[{}]",
+									mybatisConfigeFile.getAbsolutePath(),
+									environmentID);
+							return;
+						}
+					} catch (Exception e) {
+						log.warn(
+								"mybatis config file[{}] fail to get the object of FileInputStream class",
+								mybatisConfigeFile.getAbsolutePath());
+						return;
+					} finally {
+						if (null != is) {
+							try {
+								is.close();
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					}
+
+					// FIXME!
+					log.info("environmentID[{}] SqlSessionFactory registered",
+							environmentID);
+
+					// SqlSessionFactoryInfo sqlSessionFactoryInfo = new
+					// SqlSessionFactoryInfo(sqlSessionFactory, type);
+					connectionPoolName2SqlSessionFactoryHash.put(environmentID,
+							sqlSessionFactory);
+				}
+			}
+		} catch (ParserConfigurationException e1) {
+			log.warn("ParserConfigurationException::mybatis config file[{}]",
+					mybatisConfigeFile.getAbsolutePath());
+		} catch (SAXException e) {
+			log.warn("SAXException::mybatis config file[{}]",
+					mybatisConfigeFile.getAbsolutePath());
+		} catch (IOException e) {
+			log.warn("IOException::mybatis config file[{}]",
+					mybatisConfigeFile.getAbsolutePath());
+		}
+	}
+
+	/**
+	 * SqlSessionFactory 객체를 반환한다.
+	 * 
+	 * @param connectionPoolName
+	 * @return
+	 * @throws DBNotReadyException
+	 */
+	public SqlSessionFactory getSqlSessionFactory(String connectionPoolName)
+			throws DBNotReadyException {
+		synchronized (connectionPoolName2SqlSessionFactoryHash) {
+			SqlSessionFactory sqlSessionFactory = connectionPoolName2SqlSessionFactoryHash
+					.get(connectionPoolName);
+			if (null == sqlSessionFactory) {
+				String errorMessage = String.format(
+						"dbcp connection pool[%s] is not found",
+						connectionPoolName);
+				log.warn(errorMessage);
+				throw new DBNotReadyException(errorMessage);
+			}
+
+			if (isModified()) {
+				rebuild();
+				sqlSessionFactory = connectionPoolName2SqlSessionFactoryHash
+						.get(connectionPoolName);
+				if (null == sqlSessionFactory) {
+					String errorMessage = String.format(
+							"dbcp connection pool[%s] is not found",
+							connectionPoolName);
+					log.warn(errorMessage);
+					throw new DBNotReadyException(errorMessage);
+				}
+			}
+
+			return sqlSessionFactory;
+		}
+	}
+
+	private boolean isModified() {
+		for (LastModifiedFileInfo readFileInfo : lastModifiedFileInfoList) {
+			return readFileInfo.isModified();
+		}
+		return false;
+	}
+}
--- sinnori/util/SinnoriSourceBuilderUtil/src/kr/pe/sinnori/source_file_builder/ClientCodecSourceFileBuilder.java
+++ sinnori/util/SinnoriSourceBuilderUtil/src/kr/pe/sinnori/source_file_builder/ClientCodecSourceFileBuilder.java
@@ -3,31 +3,36 @@ package kr.pe.sinnori.source_file_builder;
 import kr.pe.sinnori.common.lib.CommonStaticFinalVars;
 import kr.pe.sinnori.common.lib.CommonType;
 
-
 public class ClientCodecSourceFileBuilder extends AbstractSourceFileBuildre {
-	
-	public String toString(CommonType.MESSAGE_TRANSFER_DIRECTION connectionDirectionMode, String messageID, String author) {
+
+	public String toString(
+			CommonType.MESSAGE_TRANSFER_DIRECTION connectionDirectionMode,
+			String messageID, String author) {
 		StringBuilder stringBuilder = new StringBuilder();
 		stringBuilder.append(getLincenseString());
-		
+
 		// pachage
 		stringBuilder.append(CommonStaticFinalVars.NEWLINE);
 		stringBuilder.append("package ");
 		stringBuilder.append(dynamicClassBasePackageName);
 		stringBuilder.append(messageID);
 		stringBuilder.append(";");
-		
+
 		// import
 		stringBuilder.append(CommonStaticFinalVars.NEWLINE);
 		stringBuilder.append(CommonStaticFinalVars.NEWLINE);
-		stringBuilder.append("import kr.pe.sinnori.common.exception.DynamicClassCallException;");
+		stringBuilder
+				.append("import kr.pe.sinnori.common.exception.DynamicClassCallException;");
 		stringBuilder.append(CommonStaticFinalVars.NEWLINE);
-		stringBuilder.append("import kr.pe.sinnori.common.message.codec.MessageDecoder;");
+		stringBuilder
+				.append("import kr.pe.sinnori.common.message.codec.MessageDecoder;");
 		stringBuilder.append(CommonStaticFinalVars.NEWLINE);
-		stringBuilder.append("import kr.pe.sinnori.common.message.codec.MessageEncoder;");
+		stringBuilder
+				.append("import kr.pe.sinnori.common.message.codec.MessageEncoder;");
 		stringBuilder.append(CommonStaticFinalVars.NEWLINE);
-		stringBuilder.append("import kr.pe.sinnori.common.protocol.MessageCodecIF;");
-		
+		stringBuilder
+				.append("import kr.pe.sinnori.common.protocol.MessageCodecIF;");
+
 		stringBuilder.append(CommonStaticFinalVars.NEWLINE);
 		stringBuilder.append(CommonStaticFinalVars.NEWLINE);
 		stringBuilder.append("/**");
@@ -42,76 +47,78 @@ public class ClientCodecSourceFileBuilder extends AbstractSourceFileBuildre {
 		stringBuilder.append(" *");
 		stringBuilder.append(CommonStaticFinalVars.NEWLINE);
 		stringBuilder.append(" */");
-		
+
 		// public final class EchoClientCodec implements MessageCodecIF {
 		stringBuilder.append(CommonStaticFinalVars.NEWLINE);
 		stringBuilder.append("public final class ");
 		stringBuilder.append(messageID);
 		stringBuilder.append("ClientCodec implements MessageCodecIF {");
-		
+
 		// @Override
 		stringBuilder.append(CommonStaticFinalVars.NEWLINE);
 		stringBuilder.append(CommonStaticFinalVars.NEWLINE);
 		stringBuilder.append("\t@Override");
-		
-		// public MessageDecoder getMessageDecoder() throws DynamicClassCallException {
+
+		// public MessageDecoder getMessageDecoder() throws
+		// DynamicClassCallException {
 		stringBuilder.append(CommonStaticFinalVars.NEWLINE);
-		stringBuilder.append("\tpublic MessageDecoder getMessageDecoder() throws DynamicClassCallException {");
-		
+		stringBuilder
+				.append("\tpublic MessageDecoder getMessageDecoder() throws DynamicClassCallException {");
+
 		stringBuilder.append(CommonStaticFinalVars.NEWLINE);
-		if ((connectionDirectionMode == CommonType.MESSAGE_TRANSFER_DIRECTION.FROM_ALL_TO_ALL) ||
-				(connectionDirectionMode == CommonType.MESSAGE_TRANSFER_DIRECTION.FROM_SERVER_TO_CLINET)) {
+		if ((connectionDirectionMode == CommonType.MESSAGE_TRANSFER_DIRECTION.FROM_ALL_TO_ALL)
+				|| (connectionDirectionMode == CommonType.MESSAGE_TRANSFER_DIRECTION.FROM_SERVER_TO_CLINET)) {
 			/** 디코더가 필요한 경우 */
 			stringBuilder.append("\t\treturn new ");
 			stringBuilder.append(messageID);
 			stringBuilder.append("Decoder();");
 		} else {
 			/** 디코더가 필요 없는 경우 */
-			// throw new DynamicClassCallException("SelfExn 메시지는 클라이언트에서 서버로 전달하지 않는 메시지 입니다.");
+			// throw new
+			// DynamicClassCallException("SelfExn 메시지는 클라이언트에서 서버로 전달하지 않는 메시지 입니다.");
 			stringBuilder.append("\t\tthrow new DynamicClassCallException(\"");
 			stringBuilder.append(messageID);
 			stringBuilder.append(" 메시지는 서버에서 클라이언트로 전달하지 않는 메시지 입니다.\");");
 		}
-		
-		
+
 		// }
 		stringBuilder.append(CommonStaticFinalVars.NEWLINE);
 		stringBuilder.append("\t}");
-		
+
 		// @Override
 		stringBuilder.append(CommonStaticFinalVars.NEWLINE);
 		stringBuilder.append(CommonStaticFinalVars.NEWLINE);
 		stringBuilder.append("\t@Override");
-		
-		// public MessageEncoder getMessageEncoder() throws DynamicClassCallException {
+
+		// public MessageEncoder getMessageEncoder() throws
+		// DynamicClassCallException {
 		stringBuilder.append(CommonStaticFinalVars.NEWLINE);
-		stringBuilder.append("\tpublic MessageEncoder getMessageEncoder() throws DynamicClassCallException {");
-		
-		
+		stringBuilder
+				.append("\tpublic MessageEncoder getMessageEncoder() throws DynamicClassCallException {");
+
 		stringBuilder.append(CommonStaticFinalVars.NEWLINE);
-		if ((connectionDirectionMode == CommonType.MESSAGE_TRANSFER_DIRECTION.FROM_ALL_TO_ALL) ||
-				(connectionDirectionMode == CommonType.MESSAGE_TRANSFER_DIRECTION.FROM_CLIENT_TO_SERVER)) {
+		if ((connectionDirectionMode == CommonType.MESSAGE_TRANSFER_DIRECTION.FROM_ALL_TO_ALL)
+				|| (connectionDirectionMode == CommonType.MESSAGE_TRANSFER_DIRECTION.FROM_CLIENT_TO_SERVER)) {
 			/** 인코더가 필요한 경우 */
 			stringBuilder.append("\t\treturn new ");
 			stringBuilder.append(messageID);
 			stringBuilder.append("Encoder();");
 		} else {
 			/** 인코더가 필요 없는 경우 */
-			// throw new DynamicClassCallException("SelfExn 메시지는 클라이언트에서 서버로 전달하지 않는 메시지 입니다.");
+			// throw new
+			// DynamicClassCallException("SelfExn 메시지는 클라이언트에서 서버로 전달하지 않는 메시지 입니다.");
 			stringBuilder.append("\t\tthrow new DynamicClassCallException(\"");
 			stringBuilder.append(messageID);
 			stringBuilder.append("메시지는 클라이언트에서 서버로 전달하지 않는 메시지 입니다.\");");
 		}
-		
 		// }
 		stringBuilder.append(CommonStaticFinalVars.NEWLINE);
 		stringBuilder.append("\t}");
-		
+
 		// }
 		stringBuilder.append(CommonStaticFinalVars.NEWLINE);
 		stringBuilder.append("}");
-		
+
 		return stringBuilder.toString();
 	}
-
 }
