45fbb6e10a10b8ba7bad1b03eaddaaf9ee50a17c
Won Jonghoon <k9200544@hanmail.net>
2016-12-10 22:55:27 +0900

    debian 0.0.6

--- sinnori/project/sample_base/server_build/src/main/java/main/SinnoriServerMain.java
+++ sinnori/project/sample_base/server_build/src/main/java/main/SinnoriServerMain.java
@@ -11,58 +11,59 @@ import kr.pe.sinnori.server.ServerProjectManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class SinnoriServerMain implements ProjectWorkerIF {	
+public class SinnoriServerMain {	
+	
+	public static class InitSimplebaseServer implements ProjectWorkerIF  {
+		@Override
+		public void doStartingWork(
+				ProjectPartValueObject projectPartVO) {
+			// Logger log = LoggerFactory.getLogger("kr.pe.sinnori.main");
+			/*
+			
+			boolean isDropAllTable = false;
+			String keyValueOfIsDropAlltable = System
+					.getProperty(ServerCommonStaticFinalVars.JAVA_SYSTEM_PROPERTIES_KEY_IS_DROP_ALL_TABLE);		
+			if (null != keyValueOfIsDropAlltable) {
+				 if (!keyValueOfIsDropAlltable.equals("true") && !keyValueOfIsDropAlltable.equals("false")) {
+					 log.warn("this server's java system properties variable '{}' has 'true' or 'false', this value[{}] is wrong", 
+							 ServerCommonStaticFinalVars.JAVA_SYSTEM_PROPERTIES_KEY_IS_DROP_ALL_TABLE, keyValueOfIsDropAlltable);
+				 }
+				 
+				 if (keyValueOfIsDropAlltable.equals("true")) {
+					 isDropAllTable = true;
+				 } else if (keyValueOfIsDropAlltable.equals("false")) {
+					 isDropAllTable = false;
+				 } else {
+					 log.error("this server's java system properties variable '{}' has 'true' or 'false', this value[{}] is wrong", 
+							 ServerCommonStaticFinalVars.JAVA_SYSTEM_PROPERTIES_KEY_IS_DROP_ALL_TABLE, keyValueOfIsDropAlltable);
+					 
+					 System.exit(1);
+				 }
+				
+			}	*/
+			
+
+			
+			
+			AbstractDBCommand arryDBCommand[] = {new SampleBaseDB()};
+			
+			for (int i=0;i < arryDBCommand.length; i++) {
+				AbstractDBCommand dbCommand = arryDBCommand[i];
+				dbCommand.execute();
+			}
+		}
+	}
+	
+	
 	public static void main(String argv[]) throws NotFoundProjectException {
 		Logger log = LoggerFactory.getLogger("kr.pe.sinnori.main");
 
 		try {
 			ServerProject mainServerProject = ServerProjectManager
 					.getInstance().getMainServerProject();
-			mainServerProject.startServer(new SinnoriServerMain());
+			mainServerProject.startServer(new InitSimplebaseServer());
 		} catch (Throwable e) {
 			log.warn("unknown error", e);
 		}
-	}
-
-
-	@Override
-	public void doStartingWork(
-			ProjectPartValueObject projectPartVO) {
-		// Logger log = LoggerFactory.getLogger("kr.pe.sinnori.main");
-		/*
-		
-		boolean isDropAllTable = false;
-		String keyValueOfIsDropAlltable = System
-				.getProperty(ServerCommonStaticFinalVars.JAVA_SYSTEM_PROPERTIES_KEY_IS_DROP_ALL_TABLE);		
-		if (null != keyValueOfIsDropAlltable) {
-			 if (!keyValueOfIsDropAlltable.equals("true") && !keyValueOfIsDropAlltable.equals("false")) {
-				 log.warn("this server's java system properties variable '{}' has 'true' or 'false', this value[{}] is wrong", 
-						 ServerCommonStaticFinalVars.JAVA_SYSTEM_PROPERTIES_KEY_IS_DROP_ALL_TABLE, keyValueOfIsDropAlltable);
-			 }
-			 
-			 if (keyValueOfIsDropAlltable.equals("true")) {
-				 isDropAllTable = true;
-			 } else if (keyValueOfIsDropAlltable.equals("false")) {
-				 isDropAllTable = false;
-			 } else {
-				 log.error("this server's java system properties variable '{}' has 'true' or 'false', this value[{}] is wrong", 
-						 ServerCommonStaticFinalVars.JAVA_SYSTEM_PROPERTIES_KEY_IS_DROP_ALL_TABLE, keyValueOfIsDropAlltable);
-				 
-				 System.exit(1);
-			 }
-			
-		}	*/
-		
-
-		
-		
-		AbstractDBCommand arryDBCommand[] = {new SampleBaseDB()};
-		
-		for (int i=0;i < arryDBCommand.length; i++) {
-			AbstractDBCommand dbCommand = arryDBCommand[i];
-			dbCommand.execute();
-		}
-	}
-	
-	
+	}	
 }
